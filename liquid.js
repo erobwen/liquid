ff4fb44ed205e24c8ccb42beb004251ff882793c
Robert Renbris <robert.wensman@gmail.com>
2016-04-17 07:57:58 +0200

    Better ascii art

--- liquid.js
+++ liquid.js
@@ -1,5 +1,6 @@
 
 var express = require('express');
+var colors = require('colors');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -10,12 +11,12 @@ app.listen(3000, function () {
   console.log('Liquid is now listening on port 3000!');
 });
 
-console.log(
-"  _     _             _     _ \n" +
-" | |   (_)           (_)   | |\n" +
-" | |    _  __ _ _   _ _  __| |\n" + 
-" | |   | |/ _` | | | | |/ _` |\n" +
-" | |___| | (_| | |_| | | (_| |\n" +
-" |_____|_|\\__, |\\__,_|_|\\__,_|\n" +
-"              | |              \n" +			  
-"              |_|              \n");
+console.log(colors.cyan(
+"  _     _             _     _         \n" +
+" | |   (_)           (_)   | |     /\\  \n" +
+" | |    _  __ _ _   _ _  __| |    /  \\    \n" + 
+" | |   | |/ _` | | | | |/ _` |   /    \\  \n" +
+" | |___| | (_| | |_| | | (_| |  (      )\n" +
+" |_____|_|\\__, |\\__,_|_|\\__,_|   \\_____/      \n" +
+"             | |                      \n" +			  
+"             |_|                      \n"));
