f405ca42dd5458d038aa95eef027bad20a04f7b3
Robert Renbris <robert.wensman@gmail.com>
2016-04-17 07:41:28 +0200

    First commit.

---
+++ liquid.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Liquid is now listening on port 3000!');
+});
+
+console.log(
+"  _     _             _     _ \n" +
+" | |   (_)           (_)   | |\n" +
+" | |    _  __ _ _   _ _  __| |\n" + 
+" | |   | |/ _` | | | | |/ _` |\n" +
+" | |___| | (_| | |_| | | (_| |\n" +
+" |_____|_|\\__, |\\__,_|_|\\__,_|\n" +
+"              | |              \n" +			  
+"              |_|              \n");
